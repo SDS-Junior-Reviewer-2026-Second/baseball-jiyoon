cc95fdf0c6fc0615ea9871e128263b01b64a5cb1
jiy0-0nv <rhjiyoon@yonsei.ac.kr>
2026-09-18 11:20:28 +0900

    입력값이 null일 경우 TC

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -6,11 +6,21 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GameTest {
 
 	@Test
-	public void 입력값이_없을_경우() {
+	public void createGame() {
+		Game game = new Game();
+		assertNotNull(game);
+	}
+
+	@Test
+	void throwExceptionWhenInputsIsNull() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			Game game = new Game();
+			game.guess(null);
+		});
 	}
 	
 	@Test
-	public void 입력값_자리수가_세자리가_아닐�경우() {
+	public void 입력값_자리수가_세자리가_아닐_경우() {
 
 	}
 	
