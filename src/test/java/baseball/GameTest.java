d6de23fb48f720f74eb36642099c9a63e60ec5c8
jiy0-0nv <rhjiyoon@yonsei.ac.kr>
2026-09-18 11:22:58 +0900

    refact: 중복코드 제거

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,20 +1,25 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
+	private Game game;
+
+	@BeforeEach
+	void setUp() {
+		game = new Game();
+	}
 
 	@Test
 	public void createGame() {
-		Game game = new Game();
 		assertNotNull(game);
 	}
 
 	@Test
 	void throwExceptionWhenInputsIsNull() {
 		assertThrows(IllegalArgumentException.class, () -> {
-			Game game = new Game();
 			game.guess(null);
 		});
 	}
